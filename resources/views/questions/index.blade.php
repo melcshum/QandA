5960a9f007f11d3432b2847401fc16fec472973d
lcshum <melcshum@gmail.com>
2020-07-28 09:55:37 +0800

    Authorizing the question using Gate

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,35 +37,40 @@
                                 <div class="media-body">
                                     <div class="d-flex align-items-center">
                                         <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                        <div class="ml-auto">
-                                            <a href="{{ route('questions.edit', $question->id) }}"
-                                                class="btn btn-sm btn-outline-info">Edit</a>
-                                        </div>
-                                        <form class="form-delete" method="post"
-                                            action="{{ route('questions.destroy', $question->id) }}">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger"
-                                                onclick="return confirm('Are you sure?')">Delete</button>
-                                        </form>
-                                    </div>
-                                    <p class="lead">
-                                        Asked by
-                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                        <small class="text-muted">{{ $question->created_date }}</small>
-                                    </p>
-                                    {{ str_limit($question->body, 500) }}
-                                </div>
-                            </div>
-                            <hr>
-                        @endforeach
+                                        @if(Auth::user()->can('update-question', $question))
+                                            <div class="ml-auto">
+                                                <a href="{{ route('questions.edit', $question->id) }}"
+                                                    class="btn btn-sm btn-outline-info">Edit</a>
+                                            </div>
+                                        @endif
 
-                        <div class="mx-auto">
-                            {{ $questions->links() }}
+                                        @if(Auth::user()->can('delete-question', $question))
+                                            <form class="form-delete" method="post"
+                                                action="{{ route('questions.destroy', $question->id) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger"
+                                                    onclick="return confirm('Are you sure?')">Delete</button>
+                                            </form>
+                                        @endif
+                                        </div>
+                                        <p class="lead">
+                                            Asked by
+                                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                            <small class="text-muted">{{ $question->created_date }}</small>
+                                        </p>
+                                        {{ str_limit($question->body, 500) }}
+                                    </div>
+                                </div>
+                                <hr>
+                            @endforeach
+
+                            <div class="mx-auto">
+                                {{ $questions->links() }}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
