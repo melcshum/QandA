98ab8596fe197b84e025385bd8ec66559db78c11
lcshum <melcshum@gmail.com>
2020-07-29 12:10:42 +0800

    Implement update answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -34,9 +34,12 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
         //
+        $this->authorize('update', $answer);
+
+        return view('answers._edit', compact('question', 'answer'));
     }
 
     /**
@@ -46,9 +49,17 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
         //
+        $this->authorize('update', $answer);
+        // update->validate will return validated data
+        $answer->update(
+            $request->validate([
+                'body' => 'required'
+            ])
+         );
+         return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has updated successfully');
     }
 
     /**
