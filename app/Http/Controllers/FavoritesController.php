d6abbbee7ec63aa87d28fa6fe1a1d4d782c2e086
lcshum <melcshum@gmail.com>
2020-08-01 16:02:33 +0800

    Rewrite the Favorite into Vue.js Component

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -15,12 +15,19 @@ class FavoritesController extends Controller
     public function store(Question $question)
     {
         $question->favorites()->attach(auth()->id());
+        if (request()->expectsJson()) {
+            return response()->json(null, 204);
+        }
         return back();
     }
 
     public function destory(Question $question)
     {
         $question->favorites()->detach(auth()->id());
+        if (request()->expectsJson()) {
+            // execute successfuly and no returned message
+            return response()->json(null, 204);
+        }
         return back();
     }
 }
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -24,7 +24,7 @@ Vue.use(VueIziToast);
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('user-info', require('./components/UserInfo.vue'));
 Vue.component('answer', require('./components/Answer.vue'));
-
+Vue.component('favorite', require('./components/Favorite.vue'));
 
 const app = new Vue({
     el: '#app'
