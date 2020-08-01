1f1ebbd928afc3bcf99f51febc7f193bfeea6a69
lcshum <melcshum@gmail.com>
2020-08-01 14:49:51 +0800

    Beautify the flash message and confirm dialog

--- resources/assets/js/components/Answer.vue
+++ resources/assets/js/components/Answer.vue
@@ -29,36 +29,66 @@ export default {
           console.log(res);
           this.editing = false;
           this.bodyHtml = res.data.body_html;
-          alert(res.data.message);
+          this.$toast.success(res.data.message, "Success", { timeout: 3000 });
         })
         .catch((err) => {
-          console.log("yyyy" + err);
-          alert("xxxxxx" + err.response);
+          this.$toast.error(err.response.data.message, "Error", {
+            timeout: 3000,
+          });
         });
     },
 
     destory() {
-      if (confirm("Are you sure")) {
-        axios
-          .delete(this.endpoint)
-          .then((res) => {
-            $(this.$el).fadeOut(500, () => {
-              alert(res.data.message);
-            });
-          })
-          .catch((err) => {
-            alert("delete error");
-          });
-      }
+      this.$toast.question('Are you sure about that? ', "Confirm", {
+        timeout: 20000,
+        close: false,
+        overlay: true,
+        displayMode: "once",
+        id: "question",
+        zindex: 999,
+        title: "Hey",
+        message: "Are you sure about that?",
+        position: "center",
+        buttons: [
+          [
+            "<button><b>YES</b></button>",
+             (instance, toast) => {
+              axios
+                .delete(this.endpoint)
+                .then((res) => {
+                  $(this.$el).fadeOut(500, () => {
+                    //  alert(res.data.message);
+                    this.$toast.success(res.data.message, "Success", {
+                      timeout: 3000,
+                    });
+                  });
+                })
+                .catch((err) => {
+                  alert("delete error");
+                });
+
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            },
+            true,
+          ],
+          [
+            "<button>NO</button>",
+            function (instance, toast) {
+              instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+            },
+          ],
+        ],
+
+      });
     },
   },
   computed: {
     isInvalid() {
       return this.body.length < 10;
     },
-    endpoint(){
-       return `/questions/${this.questionId}/answers/${this.id}`;
-    }
+    endpoint() {
+      return `/questions/${this.questionId}/answers/${this.id}`;
+    },
   },
 };
 </script>
