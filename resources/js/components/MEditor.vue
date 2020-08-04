b9b02e541e4e32afc60d0c1965b1e056de07bd6a
lcshum <melcshum@gmail.com>
2020-08-04 15:37:29 +0800

    Implement autosize textarea

--- resources/js/components/MEditor.vue
+++ resources/js/components/MEditor.vue
@@ -10,16 +10,36 @@
         </li>
       </ul>
     </div>
-    <div class="card-body tab-conten">
+    <div class="card-body tab-content">
       <div class="tab-pane active" id="write">
         <slot></slot>
       </div>
-      <div class="tab-pane active" id="preview">Preview.....</div>
+      <div class="tab-pane" v-html="preview" id="preview"></div>
     </div>
   </div>
 </template>
 <script>
+import MarkdownIt from "markdown-it";
+import autosize from "autosize";
+const md = new MarkdownIt();
+
 export default {
   props: ["body"],
+  computed: {
+    preview() {
+      return md.render(this.body);
+    },
+  },
+  //   watch: {
+  //     body: function () {
+  //     //  console.log("watch body");
+  //     },
+  //   },
+  mounted() {
+    autosize(this.$el.querySelector("textarea"));
+  },
+  updated() {
+    autosize(this.$el.querySelector("textarea"));
+  },
 };
 </script>
