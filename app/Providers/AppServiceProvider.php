bc9ae6ae251c73acf1b845eb1c2ebae961de6d9c
lcshum <melcshum@gmail.com>
2020-08-04 10:22:39 +0800

    Force https in prodution

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (config('app.env') === 'production') {
-
+            \URL::forceScheme('https');
         }
     }
 
